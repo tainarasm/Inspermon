49c7abb0e44ff4e68d5cb5afbd22c21e69efbbaa
Tainara <tainarasm98@outlook.com>
2017-03-31 10:57:43 -0300

    'fase1'

---
+++ programa.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 31 10:39:43 2017
+
+@author: nielton
+"""
+
+with open("inspermons.json", "r") as arquivo:
+    arquivo.readlines()
+   
+    
+    
