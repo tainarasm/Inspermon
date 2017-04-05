17fbc34686ebe2e3496acf063853c27ecd5a62a7
Tainara <tainarasm98@outlook.com>
2017-04-05 13:08:54 -0300

    'sorte'

--- programa.py
+++ programa.py
@@ -5,8 +5,34 @@ Created on Fri Mar 31 10:39:43 2017
 @author: nielton
 """
 
-with open("inspermons.json", "r") as arquivo:
-    arquivo.readlines()
-   
+import json
+
+def mostra_ipmon(ipmon):
+    print("Inspermon : {0}".format(ipmon["nome"]))
+    print("poder = {0}".format(ipmon["poder"]))
+    print("vida = {0}".format(ipmon["vida"]))
+    print("defesa = {0}".format(ipmon["defesa"]))
+    print("Inspercash = {0}\n".format(ipmon["Inspercash"]))
+
+with open('inspermonscommon.json') as arquivo:
+    inspermons = json.load(arquivo)
+    for ipmon in inspermons:
+        mostra_ipmon(ipmon)
+    
+"""valores = inspermons.values()
+if 'Jogador Principal' in valores:
+     = str(input("Qual é o seu nome?"))""" #Tentativa de trocar o "Jogador Principal" pelo nome do jogador
+     
+#Desenvolvendo o jogo:
+    
+import random
+#If escolha==passear:
+sorte = random.randint(0, len(inspermons))
+print("Prepare-se! Você acabou de encontrar o jogador %s! É hora de batalhar!" %(inspermons[sorte]['nome']))
+
+    
+
+
+
     
     
