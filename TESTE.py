f1b54d9d5dd5a6b883b0fa8073b95b0fe3e47dca
AntonioAndraues <juniorbrgamers@gmail.com>
2017-04-10 15:49:43 -0300

    Basic Changes

--- TESTE.py
+++ TESTE.py
@@ -3,7 +3,7 @@ import random
 import time
 
 def main():
-    """Main function that will welcome the player to the game."""
+    """DAR BOAS VINDAS AO JOGADOR"""
 
     Nome_jogador=str(input("Selecione o nome do seu personagem  "))
     print("\n\nOla, {0}".format(Nome_jogador))
@@ -23,14 +23,14 @@ def main():
         print("Porem o jogo nao e tao facil assim!!\r Ao caminhar pelo Insper voce podera se deparar com outros Inspermons e tera de batalhar com eles.")
     time.sleep(3)
     play_again = True
-    # Set up the play again loop
+    # Loop de player
     while play_again:
         winner = None
         player_health = 100
         computer_health = 100
 
-        # determine whose turn it is
-        turn = random.randint(1,2) # heads or tails
+        # determina quem joga
+        turn = random.randint(1,2) # 50%
         if turn == 1:
             player_turn = True
             computer_turn = False
@@ -45,13 +45,13 @@ def main():
 
         print("\nSua vida: ", player_health, "Vida do computador: ", computer_health)
 
-        # set up the main game loop
+    
         while (player_health != 0 or computer_health != 0):
 
-            heal_up = False # determine if heal has been used by the player. Resets false each loop.
-            miss = False # determine if the chosen move will miss.
+            heal_up = False 
+            miss = False
 
-            # create a dictionary of the possible moves and randomly select the damage it does when selected
+            # dicionario
             moves = {"Soco": random.randint(18, 25),
                      "Soco Mais forte": random.randint(10, 35),
                      "Recuperar vida": random.randint(20, 25)}
@@ -61,14 +61,14 @@ def main():
 
                 player_move = input("> ").lower()
 
-                move_miss = random.randint(1,5) # 20% of missing
+                move_miss = random.randint(1,5) # 20%
                 if move_miss == 1:
                     miss = True
                 else:
                     miss = False
 
                 if miss:
-                    player_move = 0 # player misses and deals no damage
+                    player_move = 0 # player misses 
                     print("Errou!")
                     time.sleep(1)
                 else:
@@ -81,7 +81,7 @@ def main():
                         print("\nMEGA SOCO NA CARA", player_move, " damage.")
                         time.sleep(1)
                     elif player_move in ("3", "Recuperar vida"):
-                        heal_up = True # heal activated
+                        heal_up = True
                         player_move = moves["Recuperar vida"]
                         print("\nRECUPEROU VIDA", player_move, " Recuperar vida.")
                         time.sleep(1)
@@ -90,7 +90,7 @@ def main():
                         time.sleep(1)
                         continue
 
-            else: # computer turn
+            else: # Computador
 
                 move_miss = random.randint(1,5)
                 if move_miss == 1:
@@ -99,7 +99,7 @@ def main():
                     miss = False
 
                 if miss:
-                    computer_move = 0 # the computer misses and deals no damage
+                    computer_move = 0 # erroou
                     print("O computador Errouu!")
                     time.sleep(1.5)
                 else:
@@ -108,17 +108,17 @@ def main():
                             computer_move = moves["Soco"]
                             print("\n O computador usou SOCO NA CARA ", computer_move, " damage.")
                             time.sleep(1.5)
-                        elif player_health > 35 and player_health <= 75: # computer decides whether to go big or play it safe
+                        elif player_health > 35 and player_health <= 75: 
                             imoves = ["Soco", "Soco Mais forte"]
                             imoves = random.choice(imoves)
                             computer_move = moves[imoves]
                             print("\nO computador deu Soco Mais forte ", imoves, ". It dealt ", computer_move, " damage.")
                             time.sleep(1.5)
                         elif player_health <= 35:
-                            computer_move = moves["Soco Mais forte"] # FINISH HIM!
+                            computer_move = moves["Soco Mais forte"]
                             print("\nO computador deu um Soco Mais forte ", computer_move, " damage.")                       
                             time.sleep(1.5)
-                    else: # if the computer has less than 30 health, there is a 50% chance they will heal
+                    else:#se tiver com pouca vida 50% de recuperar vida
                         heal_or_fight = random.randint(1,2) 
                         if heal_or_fight == 1:
                             heal_up = True
@@ -137,14 +137,14 @@ def main():
                                 print("\nO computador utilizou Soco Mais forte", imoves, ". It dealt ", computer_move, " damage.")
                                 time.sleep(2.5)
                             elif player_health <= 35:
-                                computer_move = moves["Soco Mais forte"] # FINISH HIM!
+                                computer_move = moves["Soco Mais forte"]
                                 print("\nO computador utulizou Soco Mais forte. Para finalizar ", computer_move, " damage.")
                                 time.sleep(2.5)
             if heal_up:
                 if player_turn:
                     player_health += player_move
                     if player_health > 100:
-                        player_health = 100 # cap max health at 100. No over healing!
+                        player_health = 100 #vida nao passa de 100
                 else:
                     computer_health += computer_move
                     if computer_health > 100:
@@ -153,7 +153,7 @@ def main():
                 if player_turn:
                     computer_health -= player_move
                     if computer_health < 0:
-                        computer_health = 0 # cap minimum health at 0
+                        computer_health = 0 # sem vida negativa
                         winner = "Player"
                         break
                 else:
@@ -165,11 +165,11 @@ def main():
 
             print("\nPlayer health: ", player_health, "Computer health: ", computer_health)
 
-            # switch turns
+            
             player_turn = not player_turn
             computer_turn = not computer_turn
 
-        # once main game while loop breaks, determine winner and congratulate
+        # quando acabar printar vencedor
 
         if winner == "Player":
             print("\nPlayer health: ", player_health, "Computer health: ", computer_health)
